2d1efcc1abf082bd4a917deac96b022e3fd42a33
ChintanVekariya9189 <vchintan9189@gmail.com>
2024-12-08 13:21:37 +0530

    Your commit message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,17 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// Define __dirname
+// Define __digit remote add origin https://github.com/your-username/your-repo-name.git
+rname;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
